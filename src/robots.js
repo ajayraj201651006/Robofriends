3f7dcb27e211aba0152d3f66fa0efec28dfbc9e2
ajayraj201651006 <ajayraj003016@gmail.com>
2018-09-19 14:15:37 +0530

    add file

---
+++ src/robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+
+  {
+    id:1,
+    name:'John Deo',
+    email:'John@gmail.com'
+  },
+
+  {
+    id:2,
+    name:'Chillar',
+    email:'chillar@gmai.com'
+  },
+
+  {
+    id:3,
+    name:'Squash',
+    email:'squash@gmail.com'
+  },
+
+  {
+    id:4,
+    name:'Maroon',
+    email:'maroon@gmail.com'
+  },
+
+  {
+    id:5,
+    name:'Thunder',
+    email:'thunder@gmail.com'
+  },
+
+  {
+    id:6,
+    name:'martin',
+    email:'martin@gmail.com'
+  },
+
+  {
+    id:7,
+    name:'garix',
+    email:'garix@gmail.com'
+  },
+
+  {
+    id:8,
+    name:'sia',
+    email:'sia@gmail.com'
+  },
+
+  {
+    id:9,
+    name:'david',
+    email:'david@gmail.com'
+  },
+
+  {
+    id:10,
+    name:'flexi',
+    email:'flexi@gmail.com'
+  },
+
+
+]
