5e07a15cb82a02c56c3eb714ce89bdca8407114c
Ajay Raj <ajayraj003016@gmail.com>
2018-09-25 00:12:12 +0530

    Update robots.js

--- src/robots.js
+++ src/robots.js
@@ -1,64 +1,63 @@
-export const robots = [
 
+  export const robots = [
   {
-    id:1,
-    name:'John Deo',
-    email:'John@gmail.com'
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
   },
-
   {
-    id:2,
-    name:'Chillar',
-    email:'chillar@gmai.com'
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
   },
-
   {
-    id:3,
-    name:'Squash',
-    email:'squash@gmail.com'
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
   },
-
   {
-    id:4,
-    name:'Maroon',
-    email:'maroon@gmail.com'
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
   },
-
   {
-    id:5,
-    name:'Thunder',
-    email:'thunder@gmail.com'
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
   },
-
   {
-    id:6,
-    name:'martin',
-    email:'martin@gmail.com'
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
   },
-
   {
-    id:7,
-    name:'garix',
-    email:'garix@gmail.com'
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
   },
-
   {
-    id:8,
-    name:'sia',
-    email:'sia@gmail.com'
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
   },
-
   {
-    id:9,
-    name:'david',
-    email:'david@gmail.com'
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
   },
-
   {
-    id:10,
-    name:'flexi',
-    email:'flexi@gmail.com'
-  },
-
-
-]
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
