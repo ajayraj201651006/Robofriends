5949e2869eafeaf29036461a22a237ef61841923
ajayraj201651006 <ajayraj003016@gmail.com>
2018-09-26 20:52:08 +0530

    create file

---
+++ src/reducers.js
@@ -0,0 +1,13 @@
+import {CHANGE_SEARCH_FIELD} from './constants.js';
+
+const intialState = {
+	searchField: ''
+}
+
+export const searchRobots = (state = intialState, action = {}) => {
+	switch(actions.type) {
+		case: CHANGE_SEARCH_FIELD:
+		return Object.assign({}, state, searchField: action.payload);
+	}
+}
+
