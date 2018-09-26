bcd6c4a85475ed637e71a73533636c84e905956a
ajayraj201651006 <ajayraj003016@gmail.com>
2018-09-26 21:56:51 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { provider } from 'react-redux';
 import { createStore } from 'redux';
+import { searchRobots } from './reducers';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
