bfaa2e21cc819290cd707a633fb89ea75ef51bb3
Ajay Raj <ajayraj003016@gmail.com>
2018-09-26 20:53:38 +0530

    create file

--- src/reducers.js
+++ src/reducers.js
@@ -8,6 +8,8 @@ export const searchRobots = (state = intialState, action = {}) => {
 	switch(actions.type) {
 		case: CHANGE_SEARCH_FIELD:
 		return Object.assign({}, state, searchField: action.payload);
+		default:
+			return state;
 	}
 }
 
