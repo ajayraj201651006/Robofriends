b6f3924eae647d31a652c9ff2230a2dbbe11b098
ajayraj201651006 <ajayraj003016@gmail.com>
2018-09-26 21:47:00 +0530

    update

--- src/reducers.js
+++ src/reducers.js
@@ -6,8 +6,10 @@ const intialState = {
 
 export const searchRobots = (state = intialState, action = {}) => {
 	switch(actions.type) {
-		case: CHANGE_SEARCH_FIELD:
-		return Object.assign({}, state, searchField: action.payload);
+		case CHANGE_SEARCH_FIELD:
+		return Object.assign({}, state, { searchField: action.payload });
+		default:
+		return state;
 	}
 }
 
