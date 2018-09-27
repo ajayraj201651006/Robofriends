d584fec08c6c248f5251cb5f9e9cdab379eadc25
ajayraj201651006 <ajayraj003016@gmail.com>
2018-09-27 22:45:31 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { searchRobots } from './reducers';
 import './index.css';
@@ -10,5 +10,7 @@ import 'tachyons';
 
 const store = createStore(searchRobots)
 
-ReactDOM.render(<App store = {store} />, document.getElementById('root'));
+ReactDOM.render(<Provider store = {store}>
+	           <App />
+	           </Provider>, document.getElementById('root'));
 registerServiceWorker();
